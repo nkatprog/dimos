5dc245f148cb9e7c05ccd84d27ec5084e03a44ad
alexlin2 <alex.lin416@outlook.com>
2025-12-17 04:15:23 +0000

    added twist stamped
    Former-commit-id: 49deb050b8d4b4ab49488ce7731d646e41de02ad [formerly ec1b8b8104c7722c226af03df5dfadf3f5853d0e]
    Former-commit-id: 9f76c6b4da4a04457411bd631b79557f49884fdc

--- dimos/msgs/geometry_msgs/__init__.py
+++ dimos/msgs/geometry_msgs/__init__.py
@@ -3,4 +3,5 @@ from dimos.msgs.geometry_msgs.PoseStamped import PoseStamped
 from dimos.msgs.geometry_msgs.Quaternion import Quaternion
 from dimos.msgs.geometry_msgs.Transform import Transform
 from dimos.msgs.geometry_msgs.Twist import Twist
+from dimos.msgs.geometry_msgs.TwistStamped import TwistStamped
 from dimos.msgs.geometry_msgs.Vector3 import Vector3, VectorLike
